cbef23a714f52d44df344b0897a89ce6d95862a4
Riley Bautista <112599167+RileyBautista@users.noreply.github.com>
2024-08-26 06:53:08 -0400

    Update projects

---
+++ projects/rng.py
@@ -0,0 +1,9 @@
+import random
+print('Random Number Generator')
+
+rngAmountMin = input('Select Minimum Value
+rngAmountMax = input('Select Maximum Value')
+
+rng = random.randint(rngAmountMin, rngAmountMax)
+
+print(rng)
