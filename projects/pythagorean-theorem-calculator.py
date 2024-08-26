535149c05047c15ef8bbb74216808fb12e5964d8
Riley Bautista <112599167+RileyBautista@users.noreply.github.com>
2024-08-26 07:25:00 -0400

    Added "leg or hypotenuse" validation

--- projects/pythagorean-theorem-calculator.py
+++ projects/pythagorean-theorem-calculator.py
@@ -7,22 +7,22 @@ print('Literally to practice my coding skills, if you need to legitimately use t
 legA = 0
 legB = 0
 hypotenuse = 0
+keepActive = True
 
-grah = input('Are you solving for hypotenuse or leg?').lower()
-
-if grah == 'hypotenuse':
-    legA = int(input('What is the value of Leg A?'))
-    legB = int(input('What is the value of Leg B'))
-    hypotenuse = (legA ** 2 + legB ** 2) ** 0.5
-    print('The value of the hypotenuse is', hypotenuse)
-elif grah == 'leg':
-    legA = int(input('What is the value of the Leg?'))
-    hypotenuse = int(input('What is the value of the Hypotenuse?'))
-    if hypotenuse < legA:
-        print('Invalid Triangle')
-    else:
-        legB = ((hypotenuse ** 2)-(legA ** 2)) ** 0.5
-        print('The value of the missing leg is', legB)
-elif grah == '0':
-    print('Leg or Hypotenuse')
-
+while keepActive == True:
+    grah = input('Are you solving for hypotenuse or leg?').lower()
+    if grah == 'hypotenuse':
+        legA = int(input('What is the value of Leg A?'))
+        legB = int(input('What is the value of Leg B'))
+        hypotenuse = (legA ** 2 + legB ** 2) ** 0.5
+        print('The value of the hypotenuse is', hypotenuse)
+    elif grah == 'leg':
+        legA = int(input('What is the value of the Leg?'))
+        hypotenuse = int(input('What is the value of the Hypotenuse?'))
+        if hypotenuse < legA:
+            print('Invalid Triangle')
+            keepActive = False
+        else:
+            legB = ((hypotenuse ** 2)-(legA ** 2)) ** 0.5
+            print('The value of the missing leg is', legB)
+            keepActive = False
